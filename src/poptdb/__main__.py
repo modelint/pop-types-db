785d23f67c8458ca99c82cb056a9e5b040c1445e
Leon Starr <leon_starr@modelint.com>
2025-07-02 10:49:02 -0700

    main runs and TypesDomain has path to the yaml file

---
+++ src/poptdb/__main__.py
@@ -0,0 +1,75 @@
+"""
+Blueprint Domain Type DB Populator
+
+"""
+import logging
+import logging.config
+import sys
+import os
+import argparse
+from pathlib import Path
+from poptdb.types_domain import TypesDomain
+from poptdb import version
+import atexit
+
+_logpath = Path("pop_typesdb.log")
+_progname = 'Blueprint Type Populator'
+
+def clean_up():
+    """Normal and exception exit activities"""
+    _logpath.unlink(missing_ok=True)
+
+
+def get_logger():
+    """Initiate the logger"""
+    log_conf_path = Path(__file__).parent / 'log.conf'  # Logging configuration is in this file
+    logging.config.fileConfig(fname=log_conf_path, disable_existing_loggers=False)
+    return logging.getLogger(__name__)  # Create a logger for this module
+
+
+# Configure the expected parameters and actions for the argparse module
+def parse(cl_input):
+    parser = argparse.ArgumentParser(description=_progname)
+    parser.add_argument('-t', '--types', action='store',
+                        help='Path to the types yaml file')
+    parser.add_argument('-D', '--debug', action='store_true',
+                        help='Debug mode'),
+    parser.add_argument('-L', '--log', action='store_true',
+                        help='Generate a diagnostic log file')
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='Print the current version of the repo populator')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Verbose messages')
+    return parser.parse_args(cl_input)
+
+
+def main():
+    # Start logging
+    logger = get_logger()
+    logger.info(f'{_progname} version: {version}')
+
+    # Parse the command line args
+    args = parse(sys.argv[1:])
+
+    if args.version:
+        # Just print the version and quit
+        print(f'{_progname} version: {version}')
+        sys.exit(0)
+
+    if not args.log:
+        # If no log file is requested, remove the log file before termination
+        atexit.register(clean_up)
+
+    # Types path specified
+    if args.types:
+        types_path = Path(args.types).resolve()
+        t = TypesDomain()
+        t.initialize(types_path=types_path)
+
+    logger.info("No problemo")  # We didn't die on an exception, basically
+    if args.verbose:
+        print("\nNo problemo")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/poptdb/types_domain.py
@@ -0,0 +1,37 @@
+""" types_domain.py -- The Types Domain"""
+
+# System
+import logging
+from pathlib import Path
+import yaml
+from typing import Any
+from contextlib import redirect_stdout
+
+def load_types(path: str | Path) -> dict[str, Any]:
+    with open(path, 'r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+
+class TypesDomain:
+    """
+    """
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(TypesDomain, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        # Avoid reinitialization if already initialized
+        if getattr(self, "_initialized", False):
+            return
+        self._initialized = True
+
+    def initialize(self, types_path: Path):
+        pass
+
+
+
+
+
